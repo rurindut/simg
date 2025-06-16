02197927fef9dc205e7cca09dc14ac98b74ae06a
rurinda <rurinda.damarani@sirclo.com>
2025-06-16 16:47:47 +0700

    display anggota chart in dashboard

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+use App\Filament\Widgets\AnggotaUsiaChart;
+use App\Filament\Widgets\AnggotaGenderChart;
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.dashboard';
+
+    public function getWidgets(): array
+    {
+        return [
+            AnggotaUsiaChart::class,
+            AnggotaGenderChart::class,
+        ];
+    }
+
+    protected function shouldShowWelcomeWidget(): bool
+    {
+        return false;
+    }
+
+    protected function shouldShowFilamentVersion(): bool
+    {
+        return false;
+    }
+}
---
+++ resources/views/filament/pages/dashboard.blade.php
@@ -0,0 +1,4 @@
+{{-- resources/views/filament/pages/dashboard.blade.php --}}
+<x-filament::page>
+    {{ $this->renderWidgets() }}
+</x-filament::page>
